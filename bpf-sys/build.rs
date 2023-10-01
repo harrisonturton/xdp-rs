9a74d7cda7f685871e815ace0bbc52dcddb5e677
Harry Turton <harrisonturton@gmail.com>
2023-10-01 14:48:03 +1100

    Add bpf-sys bindings to Linux BPF headers

---
+++ bpf-sys/build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::error::Error;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let out_dir = env::var("OUT_DIR")?;
+    let out_path = PathBuf::from(out_dir).join("bindings.rs");
+
+    println!("cargo:rustc-link-search=include/linux-6.5.4/");
+    println!("cargo:rerun-if-changed=include/linux-6.5.4/wrapper.h");
+
+    bindgen::Builder::default()
+        .header("include/linux-6.5.4/wrapper.h")
+        .generate_comments(false) // Valid C comments can be invalid Rust comments
+        .generate()?
+        .write_to_file(out_path)?;
+
+    Ok(())
+}
